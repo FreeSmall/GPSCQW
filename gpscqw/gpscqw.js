823d3da8ce0708bb72ffdf38b54a66a6115a35bc
zhaoyang.peng <zhaoyang.peng@tcl.com>
2016-10-17 17:01:00 +0800

    Init reposity

---
+++ gpscqw/gpscqw.js
@@ -0,0 +1,11 @@
+function MarkerPoint(){
+	alert("Hello World!");
+}
+
+	/*
+		
+	var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+		osmAttrib = '&copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> | GPS Coordinates Querry Website. contributors',
+		osm = L.tileLayer(osmUrl, {maxZoom: 18, attribution: osmAttrib});
+
+	var mapctl = L.map('mapctl').setView([31.20333, 121.59722], 8).addLayer(osm);*/
